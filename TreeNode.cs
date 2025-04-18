243d66509cc8fcabaf3a3315e341628a70c24426
ROMA <romanchep2006@gmail.com>
2025-04-18 23:25:25 +0700

    добавлен обобщённый узел TreeNode<T> с поддержкой связей и данных

---
+++ TreeNode.cs
@@ -0,0 +1,12 @@
+public class TreeNode<T> where T : IComparable<T>
+{
+  public T Data { get; set; }
+  public TreeNode<T> Left { get; set; }
+  public TreeNode<T> Right { get; set; }
+  public TreeNode<T> Parent { get; set; }
+
+  public TreeNode(T data)
+  {
+    Data = data;
+  }
+}
